117dfc17708538fff9bfafdc1344c47a286b294d
taliayit <34692076+taliayit@users.noreply.github.com>
2021-05-07 13:57:23 +0300

    install and initialize parse

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Parse from 'parse';
+
+Parse.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
+Parse.initialize(
+  'CFAyCh9JBLE2MyRJbqOxQUZP5MuO5ghN941jD3ac', // This is your Application ID
+  'kK3tXQNnm7aqMHBO0E5X2WAIvc6iBx95VQ73a8L7' // This is your Javascript key
+);
 
 ReactDOM.render(
   <React.StrictMode>
