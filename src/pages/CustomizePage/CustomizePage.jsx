032e962c96148cb67676669f719d7ff170740b4c
taliayit <34692076+taliayit@users.noreply.github.com>
2021-05-09 10:01:44 +0300

    add customize page skeleton layout

--- src/pages/CustomizePage/CustomizePage.jsx
+++ src/pages/CustomizePage/CustomizePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import { Redirect } from 'react-router-dom';
 
 function CustomizePage({activeUser}) {
@@ -8,8 +9,33 @@ function CustomizePage({activeUser}) {
     }
     
     return (
-        <div>
-            CustomizePage
+        <div className="p-customize">
+            <Container>
+                <Row><h2 className="bold-text m-auto">Customize Workout</h2></Row>
+                <Row>
+                    <Col className="p-5 text-left">
+                        <h4>Level</h4>
+                        <div className="display-flex">
+                            <img src="" alt=""/>
+                            <img src="" alt=""/>
+                            <img src="" alt=""/>
+                        </div>
+
+                        <h4>Time</h4>
+                        {/* insert time picker here */}
+
+                        <h4>Muscles</h4>
+                        <p>Select body areas on the right</p>
+
+                        <Button id="red-btn">
+                           Lets Start
+                        </Button>
+                    </Col>
+                    <Col className="p-5">
+                        {/* insert muscle selector here */}
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
