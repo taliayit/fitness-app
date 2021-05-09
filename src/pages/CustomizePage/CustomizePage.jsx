39590e90d7dc802e6c7b8cfae3774f13951c5fa9
taliayit <34692076+taliayit@users.noreply.github.com>
2021-05-09 14:24:31 +0300

    handle level selection

--- src/pages/CustomizePage/CustomizePage.jsx
+++ src/pages/CustomizePage/CustomizePage.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import { Redirect } from 'react-router-dom';
-import logo from '../../assets/images/logo_black.png';
+import logo_black from '../../assets/images/logo_black.png';
+import logo_red from '../../assets/images/logo_red.png';
 import MusclesSelector from '../../components/MusclesSelector/MusclesSelector';
 import TimePicker from '../../components/TimePicker/TimePicker';
 import './CustomizePage.css';
 
 function CustomizePage({activeUser}) {
     const [time, setTime] = useState('00:00');
+    const [level, setLevel] = useState(0);
 
     if (!activeUser) {
         return <Redirect to="/"/>
@@ -19,20 +21,20 @@ function CustomizePage({activeUser}) {
                 <Row><h2 className="bold-text m-auto">Customize Workout</h2></Row>
                 <Row>
                     <Col className="left-col">
-                        <h4>Level</h4>
+                        <h4 className="bold-text">Level</h4>
                         <div className="level-img-wrapper">
-                            <img className="logo-sm" src={logo} alt="muscle"/>
-                            <img className="logo-md" src={logo} alt="muscle"/>
-                            <img className="logo-lg" src={logo} alt="muscle"/>
+                            <img className="logo-sm" src={level === 1 ? logo_red : logo_black} onClick={() => setLevel(1)} alt="muscle"/>
+                            <img className="logo-md" src={level === 2 ? logo_red : logo_black} onClick={() => setLevel(2)} alt="muscle"/>
+                            <img className="logo-lg" src={level === 3 ? logo_red : logo_black} onClick={() => setLevel(3)} alt="muscle"/>
                         </div>
 
-                        <h4>Time<span className="time-format">(HH : mm)</span></h4>
+                        <h4 className="bold-text">Time<span className="time-format">(HH : mm)</span></h4>
                         
                         <div className="time-wrapper">
                             <TimePicker/>
                         </div>
 
-                        <h4>Muscles</h4>
+                        <h4 className="bold-text">Muscles</h4>
                         <p>Select body areas on the right</p>
 
                         <Button id="red-btn" className="mt-auto m-0">
@@ -41,7 +43,6 @@ function CustomizePage({activeUser}) {
                     </Col>
                     <Col>
                         {/* <MusclesSelector/> */}
-                        {/* insert muscle selector here */}
                     </Col>
                 </Row>
             </Container>
