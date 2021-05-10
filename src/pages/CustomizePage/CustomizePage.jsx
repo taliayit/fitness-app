05f304608087db45637d19f117380ebd49dc7520
taliayit <34692076+taliayit@users.noreply.github.com>
2021-05-10 15:59:59 +0300

    conditional link on valid submission + error message + design

--- src/pages/CustomizePage/CustomizePage.jsx
+++ src/pages/CustomizePage/CustomizePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import { Redirect, Link } from 'react-router-dom';
 import logo_black from '../../assets/images/logo_black.png';
@@ -11,19 +11,30 @@ function CustomizePage({activeUser, onSubmit}) {
     const [level, setLevel] = useState(0);
     const [time, setTime] = useState(0);
     const [muscles, setMuscles] = useState([]);
+    const [isValidToSubmit, setIsValidToSubmit] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+
+    useEffect(() => {
+        setIsValidToSubmit(level !== "0" && time > 0 && muscles.length > 0);
+    }, [level, time, muscles])
 
     if (!activeUser) {
         return <Redirect to="/"/>
     }
-    
+
+    function handleInvalidSubmit(e) {
+        e.preventDefault();
+        setErrorMessage("Please select all preferences.")
+    }
+
     return (
         <div className="p-customize">
             <Container>
                 <Row><h2 className="bold-text m-auto">Customize Workout</h2></Row>
                 <Row>
-                    <Col className="left-col">
+                    <Col className="left-col" md={5}>
                         <h4 className="bold-text">Level</h4>
-                        <div className="level-img-wrapper">
+                        <div className="level-wrapper">
                             <img className="logo-sm" src={level === 1 ? logo_red : logo_black} onClick={() => setLevel(1)} alt="muscle"/>
                             <img className="logo-md" src={level === 2 ? logo_red : logo_black} onClick={() => setLevel(2)} alt="muscle"/>
                             <img className="logo-lg" src={level === 3 ? logo_red : logo_black} onClick={() => setLevel(3)} alt="muscle"/>
@@ -36,17 +47,26 @@ function CustomizePage({activeUser, onSubmit}) {
                         </div>
 
                         <h4 className="bold-text">Muscles</h4>
-                        <p>Select body areas on the right</p>
+                        <div className="muscles-wrapper">
+                            <p>Select body areas on the right</p>
+                        </div>
+                        
+                        {errorMessage && (<div className="error-wrapper"><span className="error-icon">!</span><p className="error-msg"> {errorMessage} </p></div>)}
 
-                        <Link 
-                            to="/plan" 
+                        {isValidToSubmit ? <Link 
+                            to="/plan"
                             id="red-btn"
                             onClick={() => onSubmit({level: level, time: time, muscles: muscles})}>
                             Let's Start
                         </Link>
+                        : <Link 
+                            id="red-btn"
+                            onClick={e => handleInvalidSubmit(e)}>
+                            Let's Start
+                        </Link>}
 
                     </Col>
-                    <Col className="p-0">
+                    <Col className="p-0" md={7}>
                         <MusclesSelector onMusclesSelected={setMuscles}/>
                     </Col>
                 </Row>
