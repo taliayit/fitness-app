3f8881a06b683d8e99ee9f3d9820072ea6c9a94f
taliayit <34692076+taliayit@users.noreply.github.com>
2021-05-12 20:10:07 +0300

    implement createPlan function

--- src/pages/PlanPage/PlanPage.jsx
+++ src/pages/PlanPage/PlanPage.jsx
@@ -7,7 +7,8 @@ import './PlanPage.css';
 
 function PlanPage({preferences}) {
     const [exercises, setExercises] = useState(null);
-    
+    const [workoutPlan, setWorkoutPlan] = useState(null);
+
     useEffect(() => {
         if(preferences) {
             let allMuscles = preferences.muscles.join();
@@ -15,9 +16,9 @@ function PlanPage({preferences}) {
             async function getExercises() {
                 let url = `https://wger.de/api/v2/exercise/?format=json&language=2&limit=200&muscles=${allMuscles}`;
                 const response = await axios.get(url);
-                let dataExercises = response.data.results;
-
+                const dataExercises = response.data.results;
                 let exercisesModelArray = []; 
+
                 for(const e of dataExercises) {
                     const res = await axios.get(`https://wger.de/api/v2/exerciseinfo/${e.id}`);
                     const exerciseInfo = res.data;
@@ -33,15 +34,39 @@ function PlanPage({preferences}) {
 
     useEffect(() => {
         console.log(exercises);
+        if(exercises) {
+            createPlan();
+        }
     }, [exercises]);
+
+    useEffect(() => {
+        console.log(workoutPlan);
+    }, [workoutPlan]);
 
     if (!preferences) {
         return <Redirect to="/"/>
     }
 
+    function createPlan() {
+        let planArr = [];
+        if(exercises.length > preferences.time) {
+            planArr = exercises.slice(0, preferences.time);
+        }
+        else if(exercises.length < preferences.time){
+            while(planArr.length + exercises.length < preferences.time) {
+                planArr = planArr.concat(exercises);
+            }
+            planArr = planArr.concat(exercises.splice(0, preferences.time - planArr.length));
+        }
+        else {
+            planArr = exercises;
+        }
+        setWorkoutPlan(planArr);
+    }
+
     return (
         <div className="p-plan-page">
-            {exercises ? 
+            {exercises && workoutPlan ? 
                 <div>
                     
                 </div>
