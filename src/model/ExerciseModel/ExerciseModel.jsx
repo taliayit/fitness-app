393e8cfcdaad7665e25ce40eb05157c2cd3a47bb
taliayit <34692076+taliayit@users.noreply.github.com>
2021-05-12 18:52:28 +0300

    fetch data from Wger DB + create an array of exercise model

--- src/model/ExerciseModel/ExerciseModel.jsx
+++ src/model/ExerciseModel/ExerciseModel.jsx
@@ -1,9 +1,14 @@
 
 export default class ExerciseModel {
-    constructor(parseExercise) {
-        this.id = parseExercise.id;
-        this.name = parseExercise.get("name");
-        this.image = parseExercise.get("image");
-        this.muscles = parseExercise.get("muscles");
+    // constructor(parseExercise) {
+    //     this.id = parseExercise.id;
+    //     this.name = parseExercise.get("name");
+    //     this.image = parseExercise.get("image");
+    //     this.muscles = parseExercise.get("muscles");
+    // }
+    constructor(id, name, image) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
     }
 }
--- src/pages/PlanPage/PlanPage.jsx
+++ src/pages/PlanPage/PlanPage.jsx
@@ -1,31 +1,44 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
+import ExerciseModel from '../../model/ExerciseModel/ExerciseModel';
 
 function PlanPage({preferences}) {
     const [exercises, setExercises] = useState(null);
+    
+    useEffect(() => {
+        if(preferences) {
+            let allMuscles = preferences.muscles.join();
+
+            async function getExercises() {
+                let url = `https://wger.de/api/v2/exercise/?format=json&language=2&limit=200&muscles=${allMuscles}`;
+                const response = await axios.get(url);
+                let dataExercises = response.data.results;
+
+                let exercisesModelArray = []; 
+                for(const e of dataExercises) {
+                    const res = await axios.get(`https://wger.de/api/v2/exerciseinfo/${e.id}`);
+                    const exerciseInfo = res.data;
+                    if(exerciseInfo.images.length > 0) {
+                        exercisesModelArray.push(new ExerciseModel(exerciseInfo.id, exerciseInfo.name, exerciseInfo.images[0].image));
+                    }
+                }
+                setExercises(exercisesModelArray);
+            }
+            getExercises();
+        }
+    }, []);
 
     useEffect(() => {
-        // console.log(preferences);
-        let allMuscles = preferences.muscles.join();
-
-        async function getExercises() {
-            let url = `https://wger.de/api/v2/exercise/?format=json&limit=200&muscles=${allMuscles}`;
-            const response = await axios.get(url);
-            let dataExercises = response.data.results;
-            console.log(dataExercises)
-        }
-        getExercises();
-
-    }, []);
+        console.log(exercises);
+    }, [exercises]);
 
     if (!preferences) {
         return <Redirect to="/"/>
     }
 
     return (
-        <div>
-            
+        <div className="p-plan-page">
         </div>
     );
 }
