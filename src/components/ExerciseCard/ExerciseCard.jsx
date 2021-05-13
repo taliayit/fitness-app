0769793e825828c7e3b6e9c85f4a662e7aff8697
taliayit <34692076+taliayit@users.noreply.github.com>
2021-05-13 16:05:21 +0300

    add blured style to  disabled view when user is not logged in

--- src/components/ExerciseCard/ExerciseCard.jsx
+++ src/components/ExerciseCard/ExerciseCard.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 import './ExerciseCard.css';
 
-function ExerciseCard({exercise}) {
+function ExerciseCard({exercise, blured}) {
     return (
         <div className="c-exercise-card">
-            <Card>
+            <Card className = {blured ? "blured": ""}>
                 <Card.Img variant="top" src={exercise.image}/>
                 <Card.Body>
                     <Card.Title>{exercise.name}</Card.Title>
