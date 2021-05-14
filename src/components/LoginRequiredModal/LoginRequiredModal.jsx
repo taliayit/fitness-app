1a9ca595631f91353cddfdccf0dba2e73dba487f
taliayit <34692076+taliayit@users.noreply.github.com>
2021-05-14 15:48:11 +0300

    add login required modal + design

---
+++ src/components/LoginRequiredModal/LoginRequiredModal.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './LoginRequiredModal.css';
+
+function LoginRequiredModal({show, onClose}) {
+
+    return (
+        <div className="c-login-required-modal">
+            <Modal
+                show={show}
+                onHide={onClose}>
+                <Modal.Body>
+                    <h5>Ooops!</h5>
+                    <div>You do not have access to view this content.</div>
+                    <div>Please sign in.</div>
+
+                </Modal.Body>
+                <Modal.Footer className="justify-content-between border-0">
+                    <Button id="white-btn" onClick={onClose}>No, Thanks</Button>
+                    <Link to="/login" id="red-btn">Sign in</Link>
+                </Modal.Footer>
+            </Modal>
+
+        </div>
+    );
+}
+
+export default LoginRequiredModal;
