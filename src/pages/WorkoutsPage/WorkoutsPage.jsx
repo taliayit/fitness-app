eec734f7fd0a45596907bba5cde439bd8f803bac
taliayit <34692076+taliayit@users.noreply.github.com>
2021-05-18 11:09:13 +0300

    add workouts page layout + stylesheet

--- src/pages/WorkoutsPage/WorkoutsPage.jsx
+++ src/pages/WorkoutsPage/WorkoutsPage.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import './WorkoutsPage.css'
 
 function WorkoutsPage(props) {
+    const [workouts, setWorkouts] = useState(null);
+    
     return (
-        <div>
-            WorkoutsPage
+        <div className="p-workouts">
+            <h2 className="bold-text m-auto">My Workouts</h2>
+            {workouts ? 
+                <Container>                
+                    <Row>
+                        {workouts.map((workout, index) => 
+                            <Col key={index} lg={4} md={6} className="mt-4">
+                                {/* <WorkoutCard workout={workout}/> */}
+                            </Col>
+                        )}
+                    </Row>
+                </Container>
+                : <p className="no-data-msg">No saved workouts . . .</p>}
         </div>
     );
 }
